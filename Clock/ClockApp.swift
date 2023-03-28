ce34c9770d91c02cdc8abdbcf80c282ec99911c0
id2k1149 <id2k1149@gmail.com>
2023-03-28 12:17:21 -0400

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Max Franz Immelmann on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

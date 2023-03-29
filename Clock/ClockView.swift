4672b0167e125ca1f1c1909690ea27e7fc4b8b16
id2k1149 <id2k1149@gmail.com>
2023-03-28 21:14:49 -0400

    add comments

--- Clock/ClockView.swift
+++ Clock/ClockView.swift
@@ -12,13 +12,24 @@ struct ClockView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            let size = min(geometry.size.width, geometry.size.height)
+            // Calculate the size of the clock based on the screen width
+            let size = min(geometry.size.width, geometry.size.height) * 0.8
+            // Calculate the line width based on the clock size
             let lineWidth: CGFloat = size / 100
+            // Calculate the length of the hour hand based on the clock size
+            let hourHandLength: CGFloat = size * 0.6
+            // Calculate the length of the minute hand based on the clock size
+            let minuteHandLength: CGFloat = size * 0.72
+            // Calculate the length of the second hand based on the clock size
+            let secondHandLength: CGFloat = size * 0.725
             
             ZStack {
+                // Clock face
                 Circle()
                     .fill(Color.white)
                     .frame(width: size, height: size)
+                
+                // Hour and minute marks
                 ForEach(0..<60) { tick in
                     Rectangle()
                         .fill(Color.black)
@@ -26,11 +37,13 @@ struct ClockView: View {
                         .offset(y: -size / 2 + lineWidth / 2)
                         .rotationEffect(Angle.degrees(Double(tick) / 60 * 360))
                 }
+                
+                // Hour, minute, and second hands
                 GeometryReader { geometry in
                     // Hour Hand
                     Path { path in
                         path.move(to: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2))
-                        path.addLine(to: CGPoint(x: geometry.size.width / 2, y: size / 3))
+                        path.addLine(to: CGPoint(x: geometry.size.width / 2, y: hourHandLength))
                     }
                     .stroke(Color.black, lineWidth: lineWidth * 3)
                     .rotationEffect(Angle.degrees(Double(Calendar.current.component(.hour, from: currentTime)) / 12 * 360 + 180))
@@ -39,7 +52,7 @@ struct ClockView: View {
                     // Minute Hand
                     Path { path in
                         path.move(to: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2))
-                        path.addLine(to: CGPoint(x: geometry.size.width / 2, y: size / 2.5))
+                        path.addLine(to: CGPoint(x: geometry.size.width / 2, y: minuteHandLength))
                     }
                     .stroke(Color.black, lineWidth: lineWidth * 2)
                     .rotationEffect(Angle.degrees(Double(Calendar.current.component(.minute, from: currentTime)) / 60 * 360 + 180), anchor: .center)
@@ -47,7 +60,7 @@ struct ClockView: View {
                     // Second Hand
                     Path { path in
                         path.move(to: CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2))
-                        path.addLine(to: CGPoint(x: geometry.size.width / 2, y: size / 2.2))
+                        path.addLine(to: CGPoint(x: geometry.size.width / 2, y: secondHandLength))
                     }
                     .stroke(Color.orange, lineWidth: lineWidth)
                     .rotationEffect(Angle.degrees(Double(Calendar.current.component(.second, from: currentTime)) / 60 * 360 + 180), anchor: .center)
@@ -57,7 +70,9 @@ struct ClockView: View {
                 .frame(width: lineWidth * 2, height: size / 2)
             }
             .frame(width: size, height: size)
+            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
             .onAppear {
+                // Update the time every second
                 let timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
                     currentTime = Date()
                 }
@@ -65,6 +80,12 @@ struct ClockView: View {
             }
         }
     }
-    
-    
 }
+
+
+ struct ClockView_Previews: PreviewProvider {
+     static var previews: some View {
+        ClockView()
+     }
+ }
+ 
